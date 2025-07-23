175886b4f521c642193caf4057cca039f8242371
Creativityisall <1464103317@qq.com>
2025-07-23 10:34:35 +0800

    v8

--- conf.py
+++ conf.py
@@ -8,13 +8,34 @@ Author: Tencent AI Arena Authors
 """
 
 
-# Configuration of dimensions
-# 关于维度的配置
+# Configuration, including dimension settings, algorithm parameter settings.
+# The last few configurations in the file are for the Kaiwu platform to use and should not be changed.
+# 配置，包含维度设置，算法参数设置，文件的最后一些配置是开悟平台使用不要改动
 class Config:
+
+    # Input dimension of reverb sample on learner. Note that different algorithms have different dimensions.
+    # For example, the dimension of dqn in the sample code is 21624, and the dimension of target_dqn is 21624
+    # **Note**, this item must be configured correctly and should be aligned with the NumpyData2SampleData function data in definition.py
+    # Otherwise the sample dimension error may be reported
+    # learner上reverb样本的输入维度, 注意不同的算法维度不一样, 比如示例代码中dqn的维度是21624, target_dqn的维度是21624
+    # **注意**，此项必须正确配置，应该与definition.py中的NumpyData2SampleData函数数据对齐，否则可能报样本维度错误
+    SAMPLE_DIM = 21624
+
+    # Size of observation. After users design their own features, they should set the correct dimensions
+    # observation的维度，用户设计了自己的特征之后应该设置正确的维度
+    DIM_OF_OBSERVATION = 0
+
+    # Dimension of movement action direction
+    # 移动动作方向的维度
+    DIM_OF_ACTION_DIRECTION = 8
+
+    # Dimension of flash action direction
+    # 闪现动作方向的维度
+    DIM_OF_TALENT = 8
     LSTM_SEQ_LENGTH = 64
-    INPUT_SIZE = 88
-    OUTPUT_SIZE = 4
-    LSTM_HIDDEN_SIZE = 64
+    INPUT_SIZE = 486
+    OUTPUT_SIZE = 16
+    LSTM_HIDDEN_SIZE = 256
     LSTM_HIDDEN_LAYERS = 1
 
     GAMMA = 0.95
@@ -23,7 +44,7 @@ class Config:
     LR_ACTOR = 1e-4
     LR_CRITIC = 1e-4
     LR_LSTM = 1e-4
-    LOSS_WEIGHT = {'actor': 0.6, 'critic': 0.6, 'entropy': 1}#0.015
+    LOSS_WEIGHT = {'actor': 0.6, 'critic': 0.6, 'entropy': 0.015}#0.015
 
     # dimensionality of the sample
     # 样本维度
